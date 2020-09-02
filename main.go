25fd03d4b73e2e6d60e6b1cdca3cb7bed5cbee32
hokita <hideee.0202@gmail.com>
2020-09-02 22:02:04 +0900

    fix main.go

--- main.go
+++ main.go
@@ -7,9 +7,16 @@ import (
 	"github.com/hokita/routine/server"
 )
 
+type InMemoryStore struct{}
+
+func (i *InMemoryStore) GetPlayerScore(name string) int {
+	return 123
+}
+
 func main() {
-	var server *server.Server
-	handler := http.HandlerFunc(server.ServeHTTP)
+	s := &server.Server{Store: &InMemoryStore{}}
+
+	handler := http.HandlerFunc(s.ServeHTTP)
 	if err := http.ListenAndServe(":8080", handler); err != nil {
 		log.Fatalf("could not listen on port 8080 %v", err)
 	}
