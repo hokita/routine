af1a235ef713a6a57b98dffbc503f9d5debebd85
hokita <okita@enigmo.co.jp>
2020-10-17 09:28:37 +0900

    Add `done` column to Tasks table.

--- app/domain/task.go
+++ app/domain/task.go
@@ -5,6 +5,7 @@ import "time"
 type Task struct {
 	ID        int       `json:"id"`
 	Name      string    `json:"name"`
+	Done      bool      `json:"done"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
--- docker/postgres/init/1_create_table_tasks.sql
+++ docker/postgres/init/1_create_table_tasks.sql
@@ -1,6 +1,7 @@
 create table tasks (
     id serial primary key,
     name varchar(255) not null,
+    done boolean not null,
     created_at timestamp not null,
     updated_at timestamp not null
 );
