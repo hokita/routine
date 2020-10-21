f9ce0d47a4c495fed116daba7758fadcc3d151c1
hokita <okita@enigmo.co.jp>
2020-10-22 08:34:23 +0900

    Fix initialize queries.

--- docker/postgres/init/1_create_table_tasks.sql
+++
@@ -1,8 +0,0 @@
-create table tasks (
-    id serial primary key,
-    name varchar(255) not null,
-    done boolean not null,
-    routine_id integer not null,
-    created_at timestamp not null,
-    updated_at timestamp not null
-);
--- docker/postgres/init/2_create_table_routines.sql
+++
@@ -1,6 +0,0 @@
-create table routines (
-    id serial primary key,
-    date date not null,
-    created_at timestamp not null,
-    updated_at timestamp not null
-);
